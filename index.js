2537bc5d02931ff762c9cf23da52652edb4a8b59
abhay2132 <abhaybro768@gmail.com>
2022-08-29 21:46:00 +0530

    supports for glitch

--- index.js
+++ index.js
@@ -1,87 +1,89 @@
-(async function () {
-const fs = require("fs"),
-	repo = "https://github.com/abhay2132/np",
-	gitApi = "https://api.github.com/repos/abhay2132/np",
-	appName = "np21",
-	{ Client } = require('pg'),
-	connectionString = process.env.DATABASE_URL.replace(/\s/g, ""), 
-	client = new Client({connectionString,  ssl: { rejectUnauthorized: false }})
-//console.log(connectionString)
+(async function() {
+	const fs = require("fs"),
+		repo = "https://github.com/abhay2132/np",
+		appName = "np21",
+		{ Client } = require('pg'),
+		connectionString = process.env.DATABASE_URL.replace(/\s/g, ""),
+		client = new Client({ connectionString, ssl: { rejectUnauthorized: false } })
+	//console.log(connectionString)
 
-await client.connect()
+	await client.connect()
 
-function getRepo(cb = () => {}) {
-	console.log("downloading Repo")
-	return new Promise((res) => {
-		let { exec } = require("child_process"),
-			cmd = `rm -r np ; rm -r src; git clone ${repo} && mv np/src src && rm np -r `;
-		exec(cmd, () => res(cb()));
-	});
-}
+	function getRepo(cb = () => {}) {
+		console.log("downloading Repo")
+		return new Promise((res) => {
+			let { exec } = require("child_process"),
+				cmd = `rm -r np ; rm -r src; git clone ${repo} && mv np/src src && rm np -r `;
+			exec(cmd, () => res(cb()));
+		});
+	}
 
-function startServer() {
-	return require("./src/clusters.js")();
-}
+	function startServer() {
+		return require("./src/clusters.js")();
+	}
 
-async function getVersion() {
-	let res = await client.query("select version from app where name=$1", [appName]);
-	return res.rows[0].version;
-}
+	async function getVersion() {
+		let res = await client.query("select version from app where name=$1", [appName]);
+		return res.rows[0].version;
+	}
 
-async function isInited () {
-	let res
-	let table = "app";
-	res = await client.query("select table_name from information_schema.tables where table_name=$1", [table])
-	return res.rows.length > 0;
-}
+	async function isInited() {
+		let res
+		let table = "app";
+		res = await client.query("select table_name from information_schema.tables where table_name=$1", [table])
+		return res.rows.length > 0;
+	}
 
-async function isUpAvail () {
-	let res = await client.query("select updateavailable from app where name=$1", [appName]);
-	return res.rows[0].updateavailable;
-}
+	async function isUpAvail() {
+		let res = await client.query("select updateavailable from app where name=$1", [appName]);
+		return res.rows[0].updateavailable;
+	}
 
-async function init () {
-	await client.query("create table app ( name char(4) primary key, updateavailable boolean default false, version int default 0)")
-	await client.query("insert into app values ($1)", [appName])
-}
+	async function init() {
+		await client.query("create table app ( name char(4) primary key, updateavailable boolean default false, version int default 0)")
+		await client.query("insert into app values ($1)", [appName])
+	}
 
-async function setVersion(v = false) {
-	if ( ! v ) return v;
-	await client.query("update app set version=$1 where name=$2", [v, appName])
-}
+	async function setVersion(v = false) {
+		if (!v) return v;
+		await client.query("update app set version=$1 where name=$2", [v, appName])
+	}
 
-async function updated () {
-	await client.query("update app set updateavailable=false where name=$1", [appName])
-}
+	async function updated() {
+		await client.query("update app set updateavailable=false where name=$1", [appName])
+	}
 
-(async function () { // main function
-	try {
-	let is_inited = await isInited()
-	if ( ! is_inited ) await init();
-	let version = await getVersion(),
-		updateAvailable = (await isUpAvail() || ! fs.existsSync("src"))
-	if (updateAvailable){
+	async function updateApp(version) {
 		await getRepo();
 		await setVersion(++version);
 		await updated();
+		return version;
 	}
-	global.__appV = version;
-	console.log({is_inited, version, updateAvailable})
-	startServer();
-	} catch (e) { console.log(e)}
-})();
 
-global.__c4u = function (req, res, next) {
-	res.on("finish", async () => {
-		if ( await isUpAvail() ) {
-			setTimeout( () => process.exit("Updating App ( restarting ... )"), 500)
-		}
+	(async function() { // main function
+		try {
+			let is_inited = await isInited()
+			if (!is_inited) await init();
+			let version = await getVersion(),
+				updateAvailable = (await isUpAvail() || !fs.existsSync("src"))
+			if (updateAvailable) version = await updateApp(version);
+			global.__appV = version;
+			console.log({ is_inited, version, updateAvailable })
+			startServer();
+		} catch (e) { console.log(e) }
+	})();
+
+	global.__c4u = function(req, res, next) {
+		res.on("finish", async () => {
+			if (await isUpAvail()) {
+				setTimeout(() => process.exit("Updating App ( restarting ... )"), 500)
+			}
+		})
+		next()
+	}
+
+	process.on("exit", (m) => {
+		console.log("process exited :", m);
 	})
-	next()
-}
-
-process.on("exit", (m) => {
-	console.log("process exited :", m);
-})
 
 })();
